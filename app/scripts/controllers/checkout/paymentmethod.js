b5af4b88eb0e26e4b633d80053014beb63020955
Luis <luis19prz@gmail.com>
2016-07-07 11:27:59 -0500

    apply discount

--- app/scripts/controllers/checkout/paymentmethod.js
+++ app/scripts/controllers/checkout/paymentmethod.js
@@ -10,11 +10,14 @@
 angular.module('dashexampleApp')
   .controller('CheckoutPaymentmethodCtrl', CheckoutPaymentmethodCtrl);
 
-function CheckoutPaymentmethodCtrl($routeParams ,categoriesService, productService){
+function CheckoutPaymentmethodCtrl($routeParams, $scope, $mdMedia, $mdDialog ,categoriesService, productService){
   var vm = this;
 
   vm.selectSingle = selectSingle;
   vm.selectMultiple = selectMultiple;
+  vm.applyDeposit = applyDeposit;
+
+  vm.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
 
   vm.singlePayment = true;
   vm.multiplePayment = false;
@@ -30,6 +33,37 @@ function CheckoutPaymentmethodCtrl($routeParams ,categoriesService, productServi
     console.log('selectMultiple');
     vm.multiplePayment = true;
     vm.singlePayment = false;
+  }
+
+  function applyDeposit(ev) {
+    console.log('applyDeposit');
+    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && vm.customFullscreen;
+    $mdDialog.show({
+      controller: DepositController,
+      templateUrl: 'views/checkout/deposit-dialog.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: useFullScreen
+    })
+    .then(function(answer) {
+      vm.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      vm.status = 'You cancelled the dialog.';
+    });
+
+  }
+
+  function DepositController($scope, $mdDialog) {
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
   }
 
 
