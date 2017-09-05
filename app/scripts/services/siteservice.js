8c820b2fd5dbe813f5b024ac9d15a515d9b23e0d
Luis <luis19prz@gmail.com>
2017-09-05 16:21:49 -0500

    home css banners

--- app/scripts/services/siteservice.js
+++ app/scripts/services/siteservice.js
@@ -12,6 +12,7 @@
         findByHandle: findByHandle,
         getStoresIdMapper:getStoresIdMapper,
         test: test,
+        sortSiteBanners: sortSiteBanners
       };
 
       return service;
@@ -35,6 +36,42 @@
 
         return storesMap;
       }
+
+      function sortSiteBanners(site){
+        console.log('sortSiteBanners site', site);
+        var idsList = site.bannersOrder ? site.bannersOrder.split(',') : [];
+        var unSortedImages = [];
+        var orderedList = [];
+
+        if(!site.bannersOrder || site.bannersOrder.length === 0){
+          return site.Banners;
+        }
+
+        if(idsList.length > 0 && site.bannersOrder){
+          var files = angular.copy(site.Banners);
+          for(var i=0;i<idsList.length;i++){
+            for(var j=0; j<files.length;j++){
+              if(files[j].id === idsList[i]){
+                orderedList.push(files[j]);
+              }          
+            }
+          }
+          //Checking if a file was not in the orderedList
+          files.forEach(function(file){
+            if( !_.findWhere(orderedList, {id: file.id}) ){
+              orderedList.push(file);
+            }
+          });
+          orderedList.concat(unSortedImages);
+        }
+
+        if(orderedList.length === 0){
+          return false;
+        }
+
+        return orderedList;
+      }          
+
 
     }
 
