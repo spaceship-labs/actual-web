eb10aa984fc84c4df85d5e3dc891620cb70a15d8
Luis <luis19prz@gmail.com>
2017-10-05 18:15:55 -0500

    fix slick issue on grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -331,7 +331,7 @@ module.exports = function (grunt) {
         options: {
           collapseWhitespace: true,
           conservativeCollapse: true,
-          collapseBooleanAttributes: true,
+          collapseBooleanAttributes: false,
           removeCommentsFromCDATA: true
         },
         files: [{
--- app/scripts/siteconfig.js
+++ app/scripts/siteconfig.js
@@ -1,6 +1,7 @@
 "use strict";
 
  angular.module('siteconfig', [])
+
 .constant('SITE', {name:'actual-home',publicName:'Actual Home',baseUrl:'https://actualhome.com',domain:'actualhome.com',fb_url:'https://www.facebook.com/ActualHomeMx',instagram_url:false,foursquare_url:'https://es.foursquare.com/p/actual-home/90458136',pinterest_url:'https://es.pinterest.com/ActualGroup/'})
 
 ;
