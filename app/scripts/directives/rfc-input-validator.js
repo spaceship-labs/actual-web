6aff1e5f074702d3879929cd87c5dfba4c947497
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-03-08 17:01:19 -0500

    rfc-validate directive added

---
+++ app/scripts/directives/rfc-input-validator.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('actualApp').directive('rfcInputValidator', [
+  'clientService',
+  function(clientService) {
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+      link: function(scope, element, attrs, ctrl) {
+        function rfcValidation(ngModelValue) {
+          ctrl.$setValidity(
+            'rfcValidation',
+            clientService.validateRfc(ngModelValue)
+          );
+          return ngModelValue;
+        }
+
+        scope.$watch(attrs['ngModel'], function(newVal, oldVal) {
+          if (newVal !== oldVal || (!oldVal && !newVal)) {
+            rfcValidation(newVal);
+          }
+        });
+      }
+    };
+  }
+]);
