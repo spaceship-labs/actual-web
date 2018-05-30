fbb0ed992e5b2bbe468a45e80b0f042fdaa4c8e6
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-05-30 11:59:23 -0500

    carousel added

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -5,6 +5,7 @@ angular.module('actualWebApp').controller('HomeCtrl', HomeCtrl);
 function HomeCtrl(
   $location,
   $scope,
+  $timeout,
   $rootScope,
   api,
   siteService,
@@ -77,39 +78,47 @@ function HomeCtrl(
       direction: 'DESC'
     };
 
+    vm.top = {};
+
     if (activeStore.code !== 'actual_kids') {
       loadProducts('salas', sortOption, PRODUCTS_TO_LOAD).then(function(
         products
       ) {
+        vm.top['salas'] = products;
         vm.livingRooms = products;
       });
 
       loadProducts('comedores', sortOption, PRODUCTS_TO_LOAD).then(function(
         products
       ) {
+        vm.top['comedores'] = products;
         vm.dinningRooms = products;
       });
 
       loadProducts('recamaras', sortOption, PRODUCTS_TO_LOAD).then(function(
         products
       ) {
+        vm.top['recamaras'] = products;
         vm.bedRooms = products;
       });
     } else {
       loadProducts('ninos', sortOption, PRODUCTS_TO_LOAD).then(function(
         products
       ) {
+        vm.top['ninos'] = products;
         vm.kidsForniture = products;
       });
 
       loadProducts('comoda-infantil', sortOption, PRODUCTS_TO_LOAD).then(
         function(products) {
+          vm.top['comoda-infantil'] = products;
           vm.kidsBureaus = products;
         }
       );
 
       loadProducts('camas-infantiles', sortOption, PRODUCTS_TO_LOAD).then(
         function(products) {
+          vm.top['camas-infantiles'] = products;
           vm.kidsBeds = products;
         }
       );
@@ -139,6 +148,24 @@ function HomeCtrl(
         return productService.formatProducts(products);
       })
       .then(function(productsFormatted) {
+        $timeout(function() {
+          vm.top['salas' + '-loaded'] = true;
+        }, 500);
+        $timeout(function() {
+          vm.top['comedores' + '-loaded'] = true;
+        }, 500);
+        $timeout(function() {
+          vm.top['recamaras' + '-loaded'] = true;
+        }, 500);
+        $timeout(function() {
+          vm.top['ninos' + '-loaded'] = true;
+        }, 500);
+        $timeout(function() {
+          vm.top['comoda-infantil' + '-loaded'] = true;
+        }, 500);
+        $timeout(function() {
+          vm.top['camas-infantiles' + '-loaded'] = true;
+        }, 500);
         return productsFormatted;
       });
   }
@@ -149,6 +176,7 @@ function HomeCtrl(
 HomeCtrl.$inject = [
   '$location',
   '$scope',
+  '$timeout',
   '$rootScope',
   'api',
   'siteService',
