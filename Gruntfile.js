8ba03ee920b13d9c73b94831ea809dd4213584c4
Luis <luis19prz@gmail.com>
2018-09-11 11:57:32 -0500

    fixed gruntfile cdn url and added thumb sizes

--- Gruntfile.js
+++ Gruntfile.js
@@ -543,6 +543,7 @@ module.exports = function(grunt) {
           ENV: {
             name: 'dev',
             apiEndpoint: 'http://localhost:1337',
+            cdnUrl: 'https://d116li125og699.cloudfront.net',
             adminUrl: 'http://localhost:3000',
             tokenPrefix: 'dev',
             conektaHomeKey: 'key_LcURg3XbcFPVXud8KmHkmGg',
