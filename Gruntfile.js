e9f4d3af3a4cd1dc2025ee57295d78baa80d4e12
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2019-08-28 20:43:33 -0500

    changed api endpoint

--- Gruntfile.js
+++ Gruntfile.js
@@ -522,7 +522,7 @@ module.exports = function(grunt) {
         constants: {
           ENV: {
             name: 'production',
-            apiEndpoint: 'https://apikids.actualkids.com',
+            apiEndpoint: 'https://apikidsweb.miactual.com',
             cdnUrl: 'https://d116li125og699.cloudfront.net',
             adminUrl: 'http://adminkids.miactual.com',
             tokenPrefix: 'production',
