c1ee625a164f0ebd660d3f68103ea1adb30ee629
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2019-09-19 20:32:49 -0500

    production mp token

--- app/scripts/controllers/checkout/payments.js
+++ app/scripts/controllers/checkout/payments.js
@@ -312,8 +312,8 @@ function CheckoutPaymentsCtrl(
 
   function guessingPaymentMethod(payment) {
     Mercadopago.setPublishableKey(
-      // 'APP_USR-715cc117-cd35-41ae-a920-2f3a993e6927'
-      'TEST-b7083679-cd78-4b22-842d-9ff41b544bc2'
+      'APP_USR-715cc117-cd35-41ae-a920-2f3a993e6927'
+      // 'TEST-b7083679-cd78-4b22-842d-9ff41b544bc2'
     );
     var deferred = $q.defer();
     if (payment.type === 'transfer') {
