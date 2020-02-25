03b47a5e9443b23b4295dfaf21dd1ff344f5c868
Sergio Can <gersiopumas@gmail.com>
2020-02-25 05:51:42 -0500

    Added route to distribution

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@
 // use this if you want to recursively match all subfolders:
 // 'test/spec/**/*.js'
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
@@ -82,7 +82,7 @@ module.exports = function(grunt) {
       livereload: {
         options: {
           open: true,
-          middleware: function(connect) {
+          middleware: function (connect) {
             return [
               modRewrite([
                 '!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.jpg|\\.ttf|\\.woff|\\.mp4|\\.gif|\\.xml|\\.webm$ /index.html [L]'
@@ -102,7 +102,7 @@ module.exports = function(grunt) {
       test: {
         options: {
           port: 9001,
-          middleware: function(connect) {
+          middleware: function (connect) {
             return [
               connect.static('.tmp'),
               connect.static('test'),
@@ -314,6 +314,12 @@ module.exports = function(grunt) {
             cwd: '<%= yeoman.app %>/images',
             src: '{,*/}*.{png,jpg,jpeg,gif}',
             dest: '<%= yeoman.dist %>/images'
+          },
+          {
+            expand: true,
+            cwd: '<%= yeoman.app %>/design/png',
+            src: '{,*/}*.{png,jpg,jpeg,gif}',
+            dest: '<%= yeoman.dist %>/design/png'
           }
         ]
       }
@@ -618,7 +624,7 @@ module.exports = function(grunt) {
   grunt.registerTask(
     'serve',
     'Compile then start a connect web server',
-    function(target) {
+    function (target) {
       if (target === 'dist') {
         return grunt.task.run(['build', 'connect:dist:keepalive']);
       }
@@ -640,7 +646,7 @@ module.exports = function(grunt) {
   grunt.registerTask(
     'server',
     'DEPRECATED TASK. Use the "serve" task instead',
-    function(target) {
+    function (target) {
       grunt.log.warn(
         'The `server` task has been deprecated. Use `grunt serve` to start a server.'
       );
