cb017b4e1b2afa2055c1a93efd2c9c2e254ff126
Gibran <gibran@spaceshiplabs.com>
2023-09-12 23:11:53 -0500

    ENV

--- Gruntfile.js
+++ Gruntfile.js
@@ -552,7 +552,7 @@ module.exports = function (grunt) {
             apiEndpoint: 'http://localhost:1337',
             cdnUrl: 'https://d116li125og699.cloudfront.net',
             adminUrl: 'http://localhost:3000',
-            tokenPrefix: 'dev',
+            tokenPrefix: 'staged',
             conektaHomeKey: 'key_LcURg3XbcFPVXud8KmHkmGg',
             conektaKidsKey: 'key_FAVz7GZoqMy4PwugqgHNrag',
             conektaStudioKey: 'key_Eedd73QyEppQ3NxPgjVRxHQ'
