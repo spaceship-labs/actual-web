3aedfb6c3e34517aa8289321a07b6233197fffb8
Edgar Santamaria <rscedgar@gmail.com>
2024-02-19 13:25:45 -0500

    Settings in JavaScript variables

--- app/scripts/ofertas.js
+++ app/scripts/ofertas.js
@@ -4,14 +4,18 @@
   document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('load', function () {
       const carouselHome = document.getElementById('carouselHome');
-      const offersPageCarousel = document.getElementById('offers-page');
+      const offersPageCarousel = document.querySelectorAll(".offers-page");
       const currentPath = window.location.pathname;
       if (currentPath == '/') {
         carouselHome.style.display = 'block';
-        offersPageCarousel.style.display = 'none';
+        for (var oferta of offersPageCarousel) {
+          oferta.style.display = "none"; // Oculta los elementos
+        }
       } else if (currentPath == '/ofertas') {
         carouselHome.style.display = 'none';
-        offersPageCarousel.style.display = 'block';
+        for (var oferta of offersPageCarousel) {
+          oferta.style.display = "block"; // Oculta los elementos
+        }
       }
     })  
 });
